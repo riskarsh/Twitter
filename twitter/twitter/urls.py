51dbb1dc9d94309946b0bac2f7fe3f19073f453e
riskarsh <utkarshukla1@gmail.com>
2022-01-19 14:37:36 +0800

    Update twitter/twitter/urls.py
    Co-authored-by: Abhilash Raj <maxking@users.noreply.github.com>

--- twitter/twitter/urls.py
+++ twitter/twitter/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('twitter/',include("twitterapp.urls")),
+    path('twitter/',include('twitterapp.urls')),
 ]
